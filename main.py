94bec03cd078e8714e9b39b4ad9c42e5985f24ff
velastco <Velasco.Emmanuel12@gmail.com>
2025-06-03 16:10:06 -0700

    Added main python file
    Created a python file that gives us a random quote of the day.

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Keep going, you're doing great!",
+    "Hechale ganas viejon!",
+    "Ponte las pilas!",
+    "God will guide your way."
+]
+
+print("Quote of the day:")
+print(random.choice(quotes))
